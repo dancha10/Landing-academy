2d6ae144c91104ecd3c90b0806494e9df4c0a367
dancha10 <mdan201510@gmail.com>
2022-03-20 00:14:49 +0700

    feat: add input & textarea

---
+++ src/shared/ui/input/index.tsx
@@ -0,0 +1,25 @@
+import { FC, useState } from 'react'
+import classNames from 'classnames'
+
+import './style.scss'
+
+interface IInput {
+	placeholder: string
+	disabled?: boolean
+	validator?: Object
+}
+
+export const Input: FC<IInput> = ({ placeholder, disabled, validator }) => {
+	const [isActive, setActive] = useState(false)
+	const changedActive = (value: string) => (value ? setActive(true) : setActive(false))
+	return (
+		<input
+			type='text'
+			placeholder={placeholder}
+			className={classNames('input', { 'input--active': isActive })}
+			onChange={event => changedActive(event.target.value)}
+			disabled={disabled}
+			{...validator}
+		/>
+	)
+}
---
+++ src/shared/ui/textarea/index.tsx
@@ -0,0 +1,34 @@
+import { FC, useState } from 'react'
+import classList from 'classnames'
+
+import './style.scss'
+
+interface ITextarea {
+	placeholder: string
+	maxLength: number
+}
+
+export const Textarea: FC<ITextarea> = ({ placeholder, maxLength }) => {
+	const [isActive, setActive] = useState(false)
+	const [currentLength, setCurrentLength] = useState<number>(0)
+
+	const changedActive = (value: string): void => (value ? setActive(true) : setActive(false))
+
+	const changeValue = (value: string): void => {
+		setCurrentLength(value.length)
+		changedActive(value)
+	}
+
+	return (
+		<div className={classList('textarea', { 'textarea--active': isActive })}>
+			<textarea
+				placeholder={placeholder}
+				onChange={e => changeValue(e.target.value)}
+				maxLength={maxLength}
+			/>
+			<p className='textarea__counter'>
+				{currentLength}/{maxLength}
+			</p>
+		</div>
+	)
+}
